6ad16dbd6bca5f14929b093924c9e27a55552ad9
ThobiasVWT <t.horst@vwt.digital>
2019-12-24 14:49:36 +0100

    wait for solr core to startup before continueing test DAT-1455

--- contrib/docker/update_solr.py
+++ contrib/docker/update_solr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import json
 import requests
+import time
 from google.cloud import kms_v1
 
 # decrypt api key
@@ -22,17 +23,27 @@ headers = {
 request = requests.post(url, headers=headers)
 delete_url = 'https://ckan.test-app.vwtelecom.com/api/action/dataset_purge'
 if request.status_code == 200:
-    print("succes")
     data = json.loads(request.text)
     print(data['result'])
     for i in data['result']:
         payload = {'id': i}
         delete_request = requests.post(delete_url, json=payload, headers=headers)
-        if delete_request.status_code == 200:
-            print("deleted")
+        print(delete_request.status_code)
+        try:
+            while delete_request.status_code == 500:
+                delete_request = requests.post(delete_url, json=payload, headers=headers)
+                print(delete_request.status_code)
+                time.sleep(5)
+            else:
+                print('succeed')
+        except delete_request:
+            print("exception")
         else:
-            print(delete_request.status_code)
-            print("not deleted")
+            if delete_request.status_code == 200:
+                print("deleted")
+            else:
+                print(delete_request.status_code)
+                print("not deleted")
 else:
     print("Request failed")
     print(request.status_code)
