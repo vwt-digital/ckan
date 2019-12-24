656c4cb80e16bcf3a832fa7479b26f5854786c77
ThobiasVWT <t.horst@vwt.digital>
2019-12-24 16:18:12 +0100

    sleep from 5 to 1 second DAT-1455g

--- contrib/docker/update_solr.py
+++ contrib/docker/update_solr.py
@@ -5,7 +5,7 @@ import time
 from google.cloud import kms_v1
 
 # decrypt api key
-f = open("/workspace/ckan_api_key.enc", "r")
+f = open("ckan_api_key.enc", "rb")
 key = f.read()
 client = kms_v1.KeyManagementServiceClient()
 name = client.crypto_key_path_path('vwt-d-gew1-dat-solutions-cat', 'europe', 'ckan-api-key', 'ckan-api-key')
@@ -33,7 +33,7 @@ if request.status_code == 200:
             while delete_request.status_code == 500:
                 delete_request = requests.post(delete_url, json=payload, headers=headers)
                 print(delete_request.status_code)
-                time.sleep(5)
+                time.sleep(1)
             else:
                 print('succeed')
         except delete_request:
