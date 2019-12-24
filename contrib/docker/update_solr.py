4d147292cd2b2d60c58f0d65cac5cce484b38fcc
ThobiasVWT <t.horst@vwt.digital>
2019-12-24 12:55:43 +0100

    string had \n at the end test for fix DAT-1455

--- contrib/docker/update_solr.py
+++ contrib/docker/update_solr.py
@@ -5,7 +5,8 @@ from google.cloud import kms_v1
 
 # decrypt api key
 f = open("/workspace/ckan_api_key.enc", "r")
-key = f.read().splitlines()
+key = f.read().rstrip('\n')
+print(type(key))
 print(key)
 client = kms_v1.KeyManagementServiceClient()
 name = client.crypto_key_path_path('vwt-d-gew1-dat-solutions-cat', 'europe', 'ckan-api-key', 'ckan-api-key')
