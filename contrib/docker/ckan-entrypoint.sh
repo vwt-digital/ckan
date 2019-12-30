7f8e00d2ce61fb265e4c565d7f89f27b24b5a6e1
ThobiasVWT <t.horst@vwt.digital>
2019-12-30 17:44:47 +0100

    contrib/docker/ckan-entrypoint.sh

--- contrib/docker/ckan-entrypoint.sh
+++ contrib/docker/ckan-entrypoint.sh
@@ -67,10 +67,10 @@ fi
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
 echo "pre passed"
-exec "$@"
-#empty db
+# empty db
 python /workspace/update_solr.py
 echo "passed 1"
-#fill db again
+# fill db again
 python /workspace/Dataset_create.py
 echo "passed 2"
+exec "$@"
