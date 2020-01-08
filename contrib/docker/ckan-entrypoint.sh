72d627833dfc643ae24cc0017048d5f5a56f32b6
ThobiasVWT <t.horst@vwt.digital>
2020-01-08 10:58:29 +0100

    contrib/docker/ckan-entrypoint.sh

--- contrib/docker/ckan-entrypoint.sh
+++ contrib/docker/ckan-entrypoint.sh
@@ -66,7 +66,8 @@ fi
 
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
-"$@" $
+"$@"
+sleep(10)
 echo "pre passed"
 # empty db
 python /workspace/update_solr.py
