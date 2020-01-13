214b1116c359b4626bdfda529033774b1538be18
ThobiasVWT <t.horst@vwt.digital>
2020-01-13 13:22:43 +0100

    made function to loop through bucket and get file names DAT-1455

--- contrib/docker/update_solr.py
+++ contrib/docker/update_solr.py
@@ -17,6 +17,13 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     print("blob {} downloaded to {}. " .format(source_blob_name, destination_file_name))
 
 
+def files_in_bucket(bucket_name):
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name)
+
+    return blobs
+
+
 # decrypt api key
 f = open("/workspace/ckan_api_key.enc", "rb")
 key = f.read()
@@ -75,57 +82,59 @@ else:
         print("nothing")
 
 # download from google cloud storage
-download_blob("vwt-d-gew1-dat-solutions-cat-dcat-deployed-stg", "data_catalog.json", "/workspace/data_catalog.json")
-file = open("/workspace/data_catalog.json", "r")
-j = json.loads(file.read())
-for data in j['dataset']:
-    # Put the details of the dataset we're going to create into a dict.
-    dataDict = {
-        "name": data['identifier'],
-        "title": data['title'],
-        "notes": data['rights'],
-        "owner_org": 'dat',
-    }
-    # name is used for url and cant have uppercase or spaces so we have to replace space with _ or - and upper to lower
-    dataDict["name"] = dataDict["name"].replace("/", "_")
-    dataDict["name"] = dataDict["name"].lower()
-    # Use the json module to dump the dictionary to a string for posting.
-    url = 'https://ckan.test-app.vwtelecom.com/api/action/package_create'
-    # We'll use the package_create function to create a new dataset.
-    request = requests.post(url, json=dataDict, headers=headers)
-    try:
-        while request.status_code == 500:
-            request = requests.post(url, json=dataDict, headers=headers)
-            print(request.status_code)
-            time.sleep(1)
-        else:
-            print("success")
-    except request:
-        print("exception")
-    if request.status_code == 200:
-        print("success")
-    else:
-        print("Request failed")
-        print(request.status_code)
-        print(request.text)
-    # create resource for dataset
-    for resource in data['distribution']:
-        description = resource.get('description')
-        mediatype = resource.get('mediaType')
-
-        resourceDict = {
-            "package_id": dataDict["name"],
-            "url": resource['accessURL'],
-            "description": description,
-            "name": resource['title'],
-            "format": resource['format'],
-            "mediaType": mediatype
+file_names = files_in_bucket("vwt-d-gew1-dat-solutions-cat-dcats")
+for file in file_names:
+    download_blob("vwt-d-gew1-dat-solutions-cat-dcats", file.name, "/workspace/data_catalog.json")
+    file = open("/workspace/data_catalog.json", "r")
+    j = json.loads(file.read())
+    for data in j['dataset']:
+        # Put the details of the dataset we're going to create into a dict.
+        dataDict = {
+            "name": data['identifier'],
+            "title": data['title'],
+            "notes": data['rights'],
+            "owner_org": 'dat',
         }
-        resource_url = 'https://ckan.test-app.vwtelecom.com/api/action/resource_create'
-        resource_request = requests.post(resource_url, json=resourceDict, headers=headers)
-        if resource_request.status_code == 200:
-            print("success!")
+        # name is used for url and cant have uppercase or spaces so we have to replace space with _ or - and upper to lower
+        dataDict["name"] = dataDict["name"].replace("/", "_")
+        dataDict["name"] = dataDict["name"].lower()
+        # Use the json module to dump the dictionary to a string for posting.
+        url = 'https://ckan.test-app.vwtelecom.com/api/action/package_create'
+        # We'll use the package_create function to create a new dataset.
+        request = requests.post(url, json=dataDict, headers=headers)
+        try:
+            while request.status_code == 500:
+                request = requests.post(url, json=dataDict, headers=headers)
+                print(request.status_code)
+                time.sleep(1)
+            else:
+                print("success")
+        except request:
+            print("exception")
+        if request.status_code == 200:
+            print("success")
         else:
             print("Request failed")
-            print(resource_request.status_code)
-            print(resource_request.text)
+            print(request.status_code)
+            print(request.text)
+        # create resource for dataset
+        for resource in data['distribution']:
+            description = resource.get('description')
+            mediatype = resource.get('mediaType')
+
+            resourceDict = {
+                "package_id": dataDict["name"],
+                "url": resource['accessURL'],
+                "description": description,
+                "name": resource['title'],
+                "format": resource['format'],
+                "mediaType": mediatype
+            }
+            resource_url = 'https://ckan.test-app.vwtelecom.com/api/action/resource_create'
+            resource_request = requests.post(resource_url, json=resourceDict, headers=headers)
+            if resource_request.status_code == 200:
+                print("success!")
+            else:
+                print("Request failed")
+                print(resource_request.status_code)
+                print(resource_request.text)
