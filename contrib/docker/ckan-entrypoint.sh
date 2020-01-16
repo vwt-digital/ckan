313e3d73a884ba59866c1243fd81efb2d558ed5b
ThobiasVWT <t.horst@vwt.digital>
2020-01-16 19:36:49 +0100

    instead of path declar variable DAT-1613

--- contrib/docker/ckan-entrypoint.sh
+++ contrib/docker/ckan-entrypoint.sh
@@ -62,7 +62,7 @@ fi
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
 #nohup python -u /workspace/update_solr.py "${API_KEY}" "${CKAN_SITE_URL}" &
-ckan-paster --plugin=ckan search-index rebuild --config=/etc/ckan/std/std.ini &
+ckan-paster --plugin=ckan search-index rebuild --config="${CKAN_CONFIG}/std.ini"
 exec "$@"
 
 
