312934eae81e391bec2a37e427af6306a5ccf0c9
ThobiasVWT <t.horst@vwt.digital>
2020-01-16 10:28:02 +0100

    added api key as variable to update script DAT-1613

--- contrib/docker/ckan-entrypoint.sh
+++ contrib/docker/ckan-entrypoint.sh
@@ -61,7 +61,7 @@ fi
 
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
-nohup python -u /workspace/update_solr.py &
+nohup python -u /workspace/update_solr.py "${API_KEY}" &
 exec "$@"
 
 
