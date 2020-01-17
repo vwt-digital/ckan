e42831fb64b7c7aeb29608ad31c12281b7f6535d
ThobiasVWT <t.horst@vwt.digital>
2020-01-17 12:58:43 +0100

    removed purge part and when adding only add data resources if request of dataset returned 200 DAT-1670

--- contrib/docker/update_solr.py
+++ contrib/docker/update_solr.py
@@ -36,27 +36,27 @@ headers = {
 }
 
 # Use the json module to dump the dictionary to a string for posting.
-url = 'https://{}/api/action/package_list'.format(host)
-request = requests.post(url, headers=headers)
-delete_url = 'https://{}/api/action/dataset_purge'.format(host)
-if request.status_code == 200:
-    data = json.loads(request.text)
-    for i in data['result']:
-        payload = {'id': i}
-        delete_request = requests.post(delete_url, json=payload, headers=headers)
-        try:
-            while delete_request.status_code == 500:
-                delete_request = requests.post(delete_url, json=payload, headers=headers)
-                time.sleep(1)
-        except delete_request:
-            print(delete_request.status_code)
-else:
-    try:
-        while request.status_code == 500 or request.status_code == 503:
-            request = requests.post(url, headers=headers)
-            time.sleep(1)
-    except request:
-        print(request.status_code)
+# url = 'https://{}/api/action/package_list'.format(host)
+# request = requests.post(url, headers=headers)
+# delete_url = 'https://{}/api/action/dataset_purge'.format(host)
+# if request.status_code == 200:
+#    data = json.loads(request.text)
+#   for i in data['result']:
+#        payload = {'id': i}
+#        delete_request = requests.post(delete_url, json=payload, headers=headers)
+#       try:
+#            while delete_request.status_code == 500:
+#                delete_request = requests.post(delete_url, json=payload, headers=headers)
+#                time.sleep(1)
+#        except delete_request:
+#            print(delete_request.status_code)
+# else:
+#    try:
+#        while request.status_code == 500 or request.status_code == 503:
+#            request = requests.post(url, headers=headers)
+#            time.sleep(1)
+#    except request:
+#        print(request.status_code)
 # download from google cloud storage
 file_names = files_in_bucket("vwt-d-gew1-dat-solutions-cat-dcats")
 for file in file_names:
@@ -86,16 +86,17 @@ for file in file_names:
         except request:
             print(request.status_code)
         # create resource for dataset
-        for resource in data['distribution']:
-            description = resource.get('description')
-            mediatype = resource.get('mediaType')
-            resourceDict = {
-                "package_id": dataDict["name"],
-                "url": resource['accessURL'],
-                "description": description,
-                "name": resource['title'],
-                "format": resource['format'],
-                "mediaType": mediatype
-            }
-            resource_url = 'https://{}/api/action/resource_create'.format(host)
-            resource_request = requests.post(resource_url, json=resourceDict, headers=headers)
+        if request.status_code == 200:
+            for resource in data['distribution']:
+                description = resource.get('description')
+                mediatype = resource.get('mediaType')
+                resourceDict = {
+                    "package_id": dataDict["name"],
+                    "url": resource['accessURL'],
+                    "description": description,
+                    "name": resource['title'],
+                    "format": resource['format'],
+                    "mediaType": mediatype
+                }
+                resource_url = 'https://{}/api/action/resource_create'.format(host)
+                resource_request = requests.post(resource_url, json=resourceDict, headers=headers)
