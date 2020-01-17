188ce88a81ac6c47360d87f9de338c32347412aa
ThobiasVWT <t.horst@vwt.digital>
2020-01-17 11:36:14 +0100

    search index rebuild fast didnt work DAT-1665

--- contrib/docker/ckan-entrypoint.sh
+++ contrib/docker/ckan-entrypoint.sh
@@ -62,7 +62,7 @@ fi
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
 #nohup python -u /workspace/update_solr.py "${API_KEY}" "${CKAN_SITE_URL}" &
-ckan-paster --plugin=ckan search-index rebuild_fast --config="${CKAN_CONFIG}/production.ini"
+ckan-paster --plugin=ckan search-index rebuild --config="${CKAN_CONFIG}/production.ini"
 exec "$@"
 
 
