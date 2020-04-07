fce268f7373ec5f10dd550020d1b2905575bd464
SpringPie <bente.vandepol@gmail.com>
2020-04-07 17:45:06 +0200

    Have to upgrade the database since going back to previous (stable) version DAT-2438

--- contrib/docker-GCP/ckan-entrypoint.sh
+++ contrib/docker-GCP/ckan-entrypoint.sh
@@ -77,6 +77,6 @@ fi
 
 set_environment
 ckan-paster --plugin=ckan db upgrade -c "${CKAN_CONFIG}/production.ini"
-ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
+# ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
 ckan-paster --plugin=ckan search-index rebuild --config="${CKAN_CONFIG}/production.ini"
 exec "$@"
