8fd1a22939b7bb31e10b4d99dcafb5fc4c216861
SpringPie <bente.vandepol@gmail.com>
2020-05-06 16:26:46 +0200

    Readded rebuild DAt-2694

--- contrib/docker-GCP/ckan-entrypoint.sh
+++ contrib/docker-GCP/ckan-entrypoint.sh
@@ -77,5 +77,5 @@ fi
 
 set_environment
 ckan-paster --plugin=ckan db init -c "${CKAN_CONFIG}/production.ini"
-# ckan-paster --plugin=ckan search-index rebuild --config="${CKAN_CONFIG}/production.ini"
+ckan-paster --plugin=ckan search-index rebuild --config="${CKAN_CONFIG}/production.ini"
 exec "$@"
